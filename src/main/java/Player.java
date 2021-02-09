2bcdde46b18e42118bfd8476c84b797d0d7386c2
magsa79 <magnus.gustafsson2@hm.com>
2021-02-09 16:53:40 +0100

    added some code

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,47 @@
 public class Player {
+    private int x;
+    private int y;
+    private int oldX;
+    private int oldY;
+    private char symbol;
+
+    public Player(int x, int y, char symbol) {
+        this.x = x;
+        this.y = y;
+        this.symbol = symbol;
+        this.oldY = oldY;
+        this.oldX = oldX;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getOldX() {
+        return oldX;
+    }
+
+    public int getOldY() {
+        return oldY;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public void moveLeft(){
+        oldX = x;
+        oldY = y;
+        x -= 2;
+    }
+
+    public void moveRight(){
+        oldX = x;
+        oldY = y;
+        x += 2;
+    }
 }
--- src/main/java/SpaceBattle.java
+++ src/main/java/SpaceBattle.java
@@ -3,15 +3,69 @@ import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import java.io.IOException;
+
+
 public class SpaceBattle {
 
     public static void main(String[] args) throws Exception {
 
+        Terminal terminal = createTerminal();
+        Player player = createPlayer();
+        drawCharacters(terminal, player);
+
+        do {
+            KeyStroke keyStroke = getUserKeyStroke(terminal);
+
+            movePlayer(player, keyStroke);
+
+            drawCharacters(terminal, player);
+
+
+
+
+        } while (true);
+    }
+
+    private static Terminal createTerminal() throws IOException {
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
         Terminal terminal = terminalFactory.createTerminal();
         terminal.setCursorVisible(false);
+        return terminal;
+    }
 
+    private static Player createPlayer() {
+        return new Player(35, 22, 'A');
+    }
 
+    private static void drawCharacters(Terminal terminal, Player player) throws IOException {
 
+        terminal.setCursorPosition(player.getOldX(), player.getOldY());
+        terminal.putCharacter(' ');
+
+        terminal.setCursorPosition(player.getX(), player.getY());
+        terminal.putCharacter(player.getSymbol());
+
+        terminal.flush();
+    }
+
+    private static void movePlayer(Player player, KeyStroke keyStroke) {
+        switch (keyStroke.getKeyType()) {
+            case ArrowLeft:
+                player.moveLeft();
+                break;
+            case ArrowRight:
+                player.moveRight();
+                break;
+        }
+    }
+
+    private static KeyStroke getUserKeyStroke(Terminal terminal) throws InterruptedException, IOException {
+        KeyStroke keyStroke;
+        do {
+            Thread.sleep(5);
+            keyStroke = terminal.pollInput();
+        } while (keyStroke == null);
+        return keyStroke;
     }
 }
