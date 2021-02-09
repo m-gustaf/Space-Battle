e2db57027936ef864ad24043613ecb455bb2dece
magsa79 <magnus.gustafsson2@hm.com>
2021-02-09 14:45:09 +0100

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        Terminal terminal = terminalFactory.createTerminal();
+        terminal.setCursorVisible(false);
+
+
+    }
+}
